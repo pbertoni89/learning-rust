ae4855c323a5bc5528550843fb3ff33c925bac5c
Patrizio Bertoni <patrizio.bertoni.89@gmail.com>
2025-04-01 23:53:07 +0200

    Course ended. Location challenge

--- barron/14_08_challenge_location/begin/location/src/main.rs
+++ barron/14_08_challenge_location/begin/location/src/main.rs
@@ -1,5 +1,22 @@
 /* YOUR CODE GOES HERE */
 
+enum Location {
+    Unknown,
+    Anonymous,
+    Known(f32, f32)
+}
+
+impl Location {
+    fn display(&self) {
+        match self {
+            Location::Unknown => println!("Unknown location"),
+            Location::Anonymous => println!("Anonymous location"),
+            Location::Known(x, y) => println!("Known location at {}, {}", x, y)
+        }
+    }
+}
+
+
 fn main() {
     let address = Location::Unknown;
     address.display();
--- barron/15_03_match_result/end/match_result/src/main.rs
+++ barron/15_03_match_result/end/match_result/src/main.rs
@@ -1,9 +1,19 @@
 use std::fs;
 use std::io;
 
+fn rustrover_suggests(result: Result<String, io::Error>) -> String {
+    let contents = result.unwrap_or_else(|error| match error.kind() {
+        io::ErrorKind::NotFound => String::from("File not found."),
+        io::ErrorKind::PermissionDenied => String::from("Permission denied."),
+        _ => panic!("Another type of error: {:?}", error)
+    });
+    contents
+}
+
 fn main() {
     let result = fs::read_to_string("the_ultimate_question.txt");
 
+    //noinspection RsReplaceMatchExpr
     let contents = match result {
         Ok(message) => message,
         Err(error) => match error.kind() {
@@ -12,6 +22,9 @@ fn main() {
             _ => panic!("Another type of error: {:?}", error)
         }
     };
+
+    let result = fs::read_to_string("yet_another.txt");
+    rustrover_suggests(result);
 
     println!("contents is: {:?}", contents);
 }
